3e15353f10cb613405c2f5d48da0044810ce2506
partykyoung <partykyoung@gmail.com>
2023-12-31 01:43:53 +0900

    feat: stylex 설정

---
+++ src/commons/components/Test.tsx
@@ -0,0 +1,13 @@
+import stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  root: {
+    color: "green",
+  },
+});
+
+function Test() {
+  return <div {...stylex.props(styles.root)}>테스트</div>;
+}
+
+export default Test;
