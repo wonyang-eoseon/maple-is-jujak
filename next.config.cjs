a28bb61b5fd041a92fb8cebe7901fe24ca20c77e
조성원 <tjddnjsjo@kakao.com>
2024-02-15 16:46:07 +0900

    chore: init project

---
+++ next.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+const stylexPlugin = require('@stylexjs/nextjs-plugin');
+const babelrc = require('./.babelrc.js');
+const plugins = babelrc.plugins;
+const [_name, options] = plugins.find(
+  (plugin) => Array.isArray(plugin) && plugin[0] === '@stylexjs/babel-plugin'
+);
+const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
+const aliases = options.aliases ?? undefined;
+const useCSSLayers = options.useCSSLayers ?? undefined;
+
+module.exports = stylexPlugin({ rootDir, aliases, useCSSLayers })({
+  transpilePackages: ['@stylexjs/open-props'],
+});
